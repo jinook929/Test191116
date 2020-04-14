bdf5f3759b430c7f38d0ca0111100a38c1afa442
jinook929 <jinook929@gmail.com>
2020-04-13 21:29:23 -0400

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Test test = new Test(7, 8);
+        System.out.println(test.addition(5, 6));
+    }
+}
---
+++ src/academy/learnprogramming/Test.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+public class Test {
+    private int x;
+    private int y;
+
+    public Test(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int addition(int x, int y) {
+        return x + y;
+    }
+}
